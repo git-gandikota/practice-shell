11e66b20d3bc86fd58a036e9b503e131ea534f87
git-gandikota <ganidkota.fou@gamil.com>
2024-06-20 09:56:49 +0530

    frontend configuration

--- Frontend.sh
+++ Frontend.sh
@@ -50,8 +50,8 @@ VALIDATE $? "Navigating to html folder"
 unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Unzipping frontend code"
 
-cp /home/ec2-user/practice-shell /etc/nginx/default.d/expense.conf &>>$LOGFILE
-VALIDATE $? "Coonecting to expense.conf"
+cp /home/ec2-user/practice-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Connecting to expense.conf"
 
 systemctl restart nginx &>>$LOGFILE
 VALIDATE $? "Restarting nginx"
