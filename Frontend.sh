c3893ce74e0bc857cd151c0602ccf5ade65c6004
git-gandikota <ganidkota.fou@gamil.com>
2024-06-20 09:55:19 +0530

    frontend configuration

---
+++ Frontend.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2....$R Failed $N"
+        exit 1
+    else
+        echo -e "$2....$G suceess $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 #manually exit if error comes.
+else
+    echo "You are Super user"
+fi
+
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Removing default frontend content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading our frontend content"
+
+cd /usr/share/nginx/html &>>$LOGFILE
+VALIDATE $? "Navigating to html folder"
+
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Unzipping frontend code"
+
+cp /home/ec2-user/practice-shell /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Coonecting to expense.conf"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting nginx"
