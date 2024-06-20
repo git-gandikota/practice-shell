28f8f2594a9ac7432d726519c7caa8c73053ecb8
git-gandikota <ganidkota.fou@gamil.com>
2024-06-20 11:50:05 +0530

    checking calling function

--- Backend.sh
+++ Backend.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+source ./common.sh
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+check_root
 
-echo "Please enter DB password:"
+echo -e "$G Please enter DataBase password: $N"
 read -s mysql_root_password
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2....$R Failed $N"
-        exit 1
-    else
-        echo -e "$2....$G suceess $N"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 #manually exit if error comes.
-else
-    echo "You are Super user"
-fi
 
 dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "Disabling default nodejs"
--- MySql.sh
+++ MySql.sh
@@ -1,36 +1,11 @@
 #!/bin/bash
 
-USERID=$(id -u)
+source ./common.sh
 
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-N="\e[0m"
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
+check_root
 
 echo -e "$G Please enter DataBase password: $N"
 read -s mysql_root_password
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2...$R Failed $N"
-        exit 1
-    else
-        echo -e "$2...$G Success $N"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-    then
-        echo -e "$R Hello you are not a super user. This script has to be run with root access. $N "
-        exit 1
-    else
-        echo -e "$G Hey you are a super user, So the script is executing. $N"
-    fi
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing MySql server."
