086129ef374e676d337381aeffa7687dbdc9d87d
git-gandikota <ganidkota.fou@gamil.com>
2024-06-24 10:13:48 +0530

    confiuring database

--- mysql.sh
+++ mysql.sh
@@ -18,5 +18,11 @@ VALIDATE $? "Enabling mysql server"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting mysql server"
 
-mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-VALIDATE $? "Setting up password for database"
+mysql -h db.ramdaws.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>LOGFILE
+    VALIDATE $?"MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
