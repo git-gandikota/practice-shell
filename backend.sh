7fc47ea8af331896b4f088b3cf4e7b0e19379d7f
git-gandikota <ganidkota.fou@gamil.com>
2024-06-24 10:24:19 +0530

    confiuring backend

--- backend.sh
+++ backend.sh
@@ -25,7 +25,7 @@ unzip /tmp/backend.zip &>>$LOGFILE
 
 npm install &>>$LOGFILE
 
-cp /home/ec2-user/shell-script-2/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/practice-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 systemctl daemon-reload &>>$LOGFILE
 
